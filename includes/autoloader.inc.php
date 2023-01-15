f7cf59593cf587a87a4c45ef335fe34ac31f40b1
ali829 <alielyassraoui@gmail.com>
2023-01-15 16:35:21 +0100

    finished the ui

--- includes/autoloader.inc.php
+++ includes/autoloader.inc.php
@@ -2,14 +2,14 @@
 function autoLoader($filename)
 {
     $url = $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
-    if (strpos($url,'includes') !== false) {
+    if (strpos($url, 'includes') !== false) {
         $path = "../classes/";
-    }else{
+    } else {
         $path = "classes/";
     }
     $ext = ".class.php";
     require_once $path . $filename . $ext;
 }
-   
+
 
 spl_autoload_register('autoLoader');
--- index.php
+++ index.php
@@ -4,17 +4,41 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./dist/output.css" rel="stylesheet">
     <title>Document</title>
+
+    <style>
+
+    </style>
 </head>
-<body>
-    <h1 class="bg-green-600">hello world</h1>
-    <form action="includes/calculator.inc.php" method="post" >
-        <input type="submit" value="calculate">
-    </form>
+
+<body class="flex justify-center items-center bg-gradient-to-r from-[#140D2B] to-[#291661] h-screen">
+    <div class="bg-white/20 backdrop-blur-md w-2/3 h-max py-12 px-7 rounded-md shadow-lg">
+        <form action="" method="" class="w-full h-full flex justify-between items-center">
+            <div>
+                <input type="number" name="fnum1" id="">
+            </div>
+            <div>
+                <select name="" id="">
+                    <option value="+" selected>+</option>
+                    <option value="-">-</option>
+                    <option value="/">/</option>
+                    <option value="*">*</option>
+                </select>
+            </div>
+            <div>
+                <input type="number" name="fnum2" id="">
+            </div>
+            <div>
+                <input type="submit" value="Calculate">
+            </div>
+        </form>
+    </div>
 </body>
+
 </html>
